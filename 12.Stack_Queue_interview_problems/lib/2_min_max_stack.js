68699aa6c446cbea7ad217740a626b8996d4d3bc
ramankarki <ramankarki40@gmail.com>
2021-05-31 17:00:31 +0545

    modification of stack class with added methods min and max that returns node in O(1) time complexity

---
+++ 12.Stack_Queue_interview_problems/lib/2_min_max_stack.js
@@ -0,0 +1,151 @@
+// ============================================================================
+// Interview Problem: Constant Time Stack Max
+// ============================================================================
+//
+// -------
+// Prompt:
+// -------
+//
+// Modify the definition of the Stack class provided to create an enhanced
+// version of a Stack data structure called MinMaxStack.
+//
+// A MinMaxStack has all of the same behavior as a Stack, but can also return
+// the node with the minimum or maximum value in constant time.
+//
+// You may alter any of the original Stack's methods, including the
+// constructor.
+//
+// Values of nodes of the MinMaxStack are always guaranteed to be numbers.
+//
+//
+// ------------
+// Constraints:
+// ------------
+//
+// (1) All MinMaxStack methods must run in constant time, O(1).
+//
+//
+// --------
+// Example:
+// --------
+//
+// const minMaxStack = new MinMaxStack();
+//
+// minMaxStack.push(10);
+// minMaxStack.push(12);
+// minMaxStack.push(8);
+// minMaxStack.push(2);
+// minMaxStack.push(20);
+//
+// console.log(minMaxStack.min().value);   => 2
+// console.log(minMaxStack.max().value);   => 20
+//
+// minMaxStack.pop();
+// console.log(minMaxStack.min().value);   => 2
+// console.log(minMaxStack.max().value);   => 12
+//
+// minMaxStack.pop();
+// console.log(minMaxStack.min().value);   => 8
+// console.log(minMaxStack.max().value);   => 12
+//
+// minMaxStack.pop();
+// console.log(minMaxStack.min().value);   => 10
+// console.log(minMaxStack.max().value);   => 12
+//
+// minMaxStack.pop();
+// console.log(minMaxStack.min().value);   => 10
+// console.log(minMaxStack.max().value);   => 10
+//
+// minMaxStack.pop();
+// console.log(minMaxStack.min());   => null
+// console.log(minMaxStack.max());   => null
+//
+//
+// -----------
+// Let's code!
+// -----------
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+// Refactor the regular Stack below into a MinMaxStack!
+class MinMaxStack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+    this.minStack = [];
+    this.maxStack = [];
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+
+    // changed code from here
+    if (!this.minStack.length) {
+      this.minStack.push(newNode);
+    } else if (newNode.value <= this.minStack[this.minStack.length - 1].value) {
+      this.minStack.push(newNode);
+    }
+
+    if (!this.maxStack.length) {
+      this.maxStack.push(newNode);
+    } else if (newNode.value >= this.maxStack[this.maxStack.length - 1].value) {
+      this.maxStack.push(newNode);
+    }
+    // to here
+
+    if (!this.top) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const temp = this.top;
+      this.top = newNode;
+      this.top.next = temp;
+    }
+    return ++this.length;
+  }
+
+  pop() {
+    if (!this.top) {
+      return null;
+    }
+    const temp = this.top;
+
+    // changed code from here
+    if (temp === this.minStack[this.minStack.length - 1]) {
+      this.minStack.pop();
+    }
+
+    if (temp === this.maxStack[this.maxStack.length - 1]) {
+      this.maxStack.pop();
+    }
+    // to here
+
+    if (this.top === this.bottom) {
+      this.bottom = null;
+    }
+    this.top = this.top.next;
+    this.length--;
+    return temp;
+  }
+
+  size() {
+    return this.length;
+  }
+
+  min() {
+    return this.minStack[this.minStack.length - 1] || null;
+  }
+
+  max() {
+    return this.maxStack[this.maxStack.length - 1] || null;
+  }
+}
+
+// Forgetting something down here? => MinMaxStack
+exports.Node = Node;
+exports.MinMaxStack = MinMaxStack;
