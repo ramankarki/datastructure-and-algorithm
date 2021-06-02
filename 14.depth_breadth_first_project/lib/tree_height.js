4e901b821330c8ad03575ba4cfe05688dd2b4d91
ramankarki <ramankarki40@gmail.com>
2021-06-02 10:14:55 +0545

    used recursion to find the height of the tree

--- 14.depth_breadth_first_project/lib/tree_height.js
+++ 14.depth_breadth_first_project/lib/tree_height.js
@@ -1,22 +1,7 @@
 function treeHeight(root) {
   if (!root) return -1;
-  if (!root.left && !root.right) return 0;
 
-  let queue = [root];
-  let nodeCount = 0;
-
-  while (queue.length) {
-    let node = queue.shift();
-    nodeCount++;
-
-    if (node.left) queue.push(node.left);
-    if (node.right) queue.push(node.right);
-  }
-
-  if (nodeCount % 2 === 0) {
-    return nodeCount / 2 + 1;
-  }
-  return Math.trunc(nodeCount / 2);
+  return Math.max(treeHeight(root.left), treeHeight(root.right)) + 1;
 }
 
 module.exports = {
